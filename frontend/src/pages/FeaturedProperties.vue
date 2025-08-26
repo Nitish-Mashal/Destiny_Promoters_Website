672de27208dd504ae2db1886934faeb6a6c25686
Nitish-Mashal <nitishmashal0@gmail.com>
2025-08-26 20:39:01 +0530

    Commiting new code: Creating API's For Lisitng

--- frontend/src/pages/FeaturedProperties.vue
+++ frontend/src/pages/FeaturedProperties.vue
@@ -9,53 +9,58 @@
         <!-- Cards Grid -->
         <div class="row g-4">
             <div v-for="(property, index) in properties" :key="index" class="col-12 col-sm-6 col-lg-3">
-                <div class="card h-100 shadow-sm border-0 rounded-3 border-5">
-                    <!-- Image Section -->
-                    <div class="relative p-2 pb-0">
-                        <img :src="property.image" class="card-img-top rounded-3" alt="Property" />
-                        <!-- Badges -->
-                        <div class="absolute top-6 left-6 flex gap-2">
-                            <span v-for="(badge, idx) in property.badges" :key="idx"
-                                class="px-3 py-1 text-xs font-bold rounded-full" :class="badgeColors(badge)">
-                                {{ badge }}
-                            </span>
-                        </div>
-                    </div>
-
-                    <!-- Card Body -->
-                    <div class="card-body">
-                        <h6 class="text-red-400 font-semibold text-[13px]">{{ property.title }}</h6>
-                        <div class="card-title fw-semibold mb-1 text-[13px]">
-                            {{ property.subtitle }}
-                        </div>
-                        <div class="text-xs mb-2 d-flex align-items-center ">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
-                                stroke="currentColor" class="h-4 w-4 me-1">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
-                            </svg>
-                            {{ property.location }}
+                <!-- Wrap card with RouterLink -->
+                <router-link :to="`/ListingDetails/${property.id}`" class="text-decoration-none">
+                    <div class="card h-100 shadow-sm border-0 rounded-3 border-5">
+                        <!-- Image Section -->
+                        <div class="relative p-2 pb-0">
+                            <img :src="property.image" class="card-img-top rounded-3" alt="Property" />
+                            <!-- Badges -->
+                            <div class="absolute top-6 left-6 flex gap-2">
+                                <span v-for="(badge, idx) in property.badges" :key="idx"
+                                    class="px-3 py-1 text-xs font-bold rounded-full" :class="badgeColors(badge)">
+                                    {{ badge }}
+                                </span>
+                            </div>
                         </div>
 
-                        <!-- Features -->
-                        <div class="d-flex flex-wrap gap-3 text-sm text-secondary">
-                            <span class="d-flex align-items-center gap-2">
-                                <img src="../assets/images/Bed.png" alt="" class="h-3" />
-                                {{ property.beds }}
-                            </span>
-                            <span class="d-flex align-items-center gap-2">
-                                <img src="../assets/images/Bath.png" alt="" class="h-3" />
-                                {{ property.baths }}
-                            </span>
-                            <span class="d-flex align-items-center gap-2">
-                                <img src="../assets/images/Sqft.png" alt="" class="h-3" />
-                                {{ property.sizes }}
-                            </span>
+                        <!-- Card Body -->
+                        <div class="card-body">
+                            <h6 class="text-red-400 font-semibold text-[13px]">
+                                {{ property.title }}
+                            </h6>
+                            <div class="card-title fw-semibold mb-1 text-[13px]">
+                                {{ property.subtitle }}
+                            </div>
+                            <div class="text-xs mb-2 d-flex align-items-center ">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                                    stroke="currentColor" class="h-4 w-4 me-1">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                                </svg>
+                                {{ property.location }}
+                            </div>
+
+                            <!-- Features -->
+                            <div class="d-flex flex-wrap gap-3 text-sm text-secondary">
+                                <span class="d-flex align-items-center gap-2">
+                                    <img src="../assets/images/Bed.png" alt="" class="h-3" />
+                                    {{ property.beds }}
+                                </span>
+                                <span class="d-flex align-items-center gap-2">
+                                    <img src="../assets/images/Bath.png" alt="" class="h-3" />
+                                    {{ property.baths }}
+                                </span>
+                                <span class="d-flex align-items-center gap-2">
+                                    <img src="../assets/images/Sqft.png" alt="" class="h-3" />
+                                    {{ property.sizes }}
+                                </span>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </router-link>
             </div>
         </div>
     </div>
@@ -64,6 +69,7 @@
 <script setup>
 const properties = [
     {
+        id: 6,
         title: "Live Healthy, Live Well",
         subtitle: "Birla Apple Spire",
         location: "Nayanda Halli, Bangalore",
@@ -71,9 +77,10 @@ const properties = [
         baths: "2",
         sizes: "1241 - 1847",
         badges: ["FOR SALE"],
-        image: new URL('../assets/images/hfy_img_1.png', import.meta.url).href
+        image: new URL("../assets/images/hfy_img_1.png", import.meta.url).href,
     },
     {
+        id: 11,
         title: "A Rare Blend of Luxury, Necessity",
         subtitle: "Suvedha Luxuria",
         location: "Uttarahalli, Bangalore",
@@ -84,6 +91,7 @@ const properties = [
         image: new URL("../assets/images/hfy_img_2.png", import.meta.url).href,
     },
     {
+        id: 9,
         title: "Higher Quality Of Living",
         subtitle: "Suvedha Paradise",
         location: "Srinivasanagar, Bangalore",
@@ -94,6 +102,7 @@ const properties = [
         image: new URL("../assets/images/hfy_img_3.png", import.meta.url).href,
     },
     {
+        id: 8,
         title: "An Irresistible Living Option",
         subtitle: "Suvedha Elegant",
         location: "Banashankari, Bangalore",
--- frontend/src/pages/homesforyou.vue
+++ frontend/src/pages/homesforyou.vue
@@ -25,52 +25,51 @@
             <el-carousel :interval="5000" arrow="always" height="350px" indicator-position="none">
                 <el-carousel-item v-for="(chunk, index) in chunkedHomes" :key="'chunk-' + index">
                     <div class="container px-5">
-                        <div class="container px-5">
+                        <div class="container">
                             <div class="row justify-content-center g-4">
                                 <div class="col-md-4" v-for="(home, idx) in chunk" :key="'home-' + idx">
-                                    <div class="card h-100 shadow-sm border-0 rounded-3">
-                                        <div class="position-relative">
-                                            <img :src="home.image" class="card-img-top p-2 rounded-4"
-                                                alt="Property Image" style="height: 220px; object-fit: cover" />
-                                            <span class="position-absolute top-0 start-0 m-3 p-2 badge bg-dark">
-                                                {{ home.type === 'ready' ? 'READY TO OCCUPY' :
-                                                    home.type.toUpperCase()
-                                                }}
-                                            </span>
-                                        </div>
-                                        <div class="card-body">
-                                            <div class="card-title fw-semibold mb-1">{{ home.title }}</div>
-                                            <p class="text-xs mb-2 d-flex align-items-center">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                    stroke-width="2" stroke="currentColor" class="h-4 w-4 me-1">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
-                                                </svg>
-                                                {{ home.location }}
-                                            </p>
-                                            <div class="d-flex flex-wrap gap-3 text-xs text-secondary">
-                                                <span class="d-flex align-items-center gap-1">
-                                                    <img src="../assets/images/Bed.png" alt="" class="h-3">
-                                                    {{
-                                                        home.beds
-                                                    }}
-                                                </span>
-                                                <span class="d-flex align-items-center gap-1">
-                                                    <img src="../assets/images/Bath.png" alt="" class="h-3">
-                                                    {{
-                                                        home.baths
-                                                    }}
-                                                </span>
-                                                <span class="d-flex align-items-center gap-1">
-                                                    <img src="../assets/images/Sqft.png" alt="" class="h-3">
-                                                    {{
-                                                        home.area }}
+                                    <router-link :to="{ name: 'ListingDetails', params: { id: home.id } }"
+                                        class="text-decoration-none">
+                                        <div class="card h-100 shadow-sm border-0 rounded-3">
+                                            <div class="position-relative">
+                                                <img :src="home.image" class="card-img-top p-2 rounded-4"
+                                                    alt="Property Image" style="height: 220px; object-fit: cover" />
+                                                <span
+                                                    class="position-absolute top-0 start-0 m-4 p-2 badge bg-dark rounded-5">
+                                                    {{ home.type === 'ready' ? 'READY TO OCCUPY' :
+                                                        home.type.toUpperCase() }}
                                                 </span>
                                             </div>
+                                            <div class="card-body">
+                                                <div class="card-title fw-semibold mb-1">{{ home.title }}</div>
+                                                <p class="text-xs mb-3 d-flex align-items-center">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                        viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                                        class="h-4 w-4 me-1">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                                                    </svg>
+                                                    {{ home.location }}
+                                                </p>
+                                                <div class="d-flex flex-wrap gap-3 text-xs text-secondary">
+                                                    <span class="d-flex align-items-center gap-1">
+                                                        <img src="../assets/images/Bed.png" alt="" class="h-3"> {{
+                                                            home.beds }}
+                                                    </span>
+                                                    <span class="d-flex align-items-center gap-1">
+                                                        <img src="../assets/images/Bath.png" alt="" class="h-3"> {{
+                                                            home.baths }}
+                                                    </span>
+                                                    <span class="d-flex align-items-center gap-1">
+                                                        <img src="../assets/images/Sqft.png" alt="" class="h-3"> {{
+                                                            home.area }}
+                                                    </span>
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
+                                    </router-link>
                                 </div>
                             </div>
                         </div>
@@ -82,44 +81,38 @@
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { ref, computed, onMounted } from 'vue'
+import { ElCarousel, ElCarouselItem } from 'element-plus'
 
-const homes = [
-    {
-        title: 'Birla Apple Spire',
-        location: 'Nayanda Halli, Bangalore',
-        beds: '2 & 3 Beds',
-        baths: '2 Baths',
-        area: '1241 - 1847 sqft',
-        type: 'ready',
-        image: new URL('../assets/images/hfy_img_1.png', import.meta.url).href
-    },
-    {
-        title: 'Suvedha Luxuria',
-        location: 'Uttarahalli, Bangalore',
-        beds: '3 Beds',
-        baths: '2 Baths',
-        area: '2227 sqft',
-        type: 'sale',
-        image: new URL('../assets/images/hfy_img_2.png', import.meta.url).href
-    },
-    {
-        title: 'Suvedha Paradise',
-        location: 'Srinivasnagar, Bangalore',
-        beds: '2 & 3 Beds',
-        baths: '2 Baths',
-        area: '1075 - 1300 sqft',
-        type: 'rent',
-        image: new URL('../assets/images/hfy_img_3.png', import.meta.url).href
+const homes = ref([])
+
+onMounted(async () => {
+    try {
+        const response = await fetch("/assets/destiny_promoters_website/data/properties.json")
+        if (!response.ok) throw new Error("Failed to fetch properties.json")
+        const data = await response.json()
+
+        homes.value = data.map(project => ({
+            id: project.id,
+            title: project.name,
+            location: project.location,
+            beds: project.bhk || "-",
+            baths: project.bath || "-",
+            area: project.superBuiltUpArea,
+            type: project.status?.toLowerCase() || "unknown",
+            image: project.thumbnail
+        }))
+    } catch (err) {
+        console.error("Error loading properties:", err)
     }
-]
+})
 
-// Function to chunk homes array into groups of 3
+// ✅ Chunk directly from homes
 const chunkedHomes = computed(() => {
     const chunkSize = 3
     const chunks = []
-    for (let i = 0; i < homes.length; i += chunkSize) {
-        chunks.push(homes.slice(i, i + chunkSize))
+    for (let i = 0; i < homes.value.length; i += chunkSize) {
+        chunks.push(homes.value.slice(i, i + chunkSize))
     }
     return chunks
 })
